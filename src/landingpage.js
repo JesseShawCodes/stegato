c8189e871399986f3c10a22050f9fead127e320a
Jesse Shaw <jdshaw1987@gmail.com>
2017-12-12 12:54:32 -0500

    Added splash component

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Heading from './header.js';
 import Searchbar from './search';
+import Splash from './splash';
 
 export default function Landingpage() {
     return (
@@ -9,6 +10,7 @@ export default function Landingpage() {
                 <Heading />
             </div>
             <div id="main">
+                <Splash />
                 <Searchbar />
             </div>
         </main>
---
+++ src/splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './splash.css';
+
+export default function Searchbar() {
+    return (
+        <div>
+            <div className="splash">
+            <img src="https://images.unsplash.com/photo-1483412033650-1015ddeb83d1?auto=format&fit=crop&w=1506&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" className="splash-image" alt="Shelf of records"></img>
+            </div>
+        </div>
+    );
+}
