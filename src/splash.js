5b29d79eebc20364b6a5ff388260fb4b03b90bd0
Jesse Shaw <jdshaw1987@gmail.com>
2017-12-13 11:44:15 -0500

    Style Updates

--- src/splash.js
+++ src/splash.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './splash.css';
 
-export default function Searchbar() {
+export default function Splash() {
     return (
-        <div>
-            <div className="splash">
+        <div className="splash">
             <img src="https://images.unsplash.com/photo-1483412033650-1015ddeb83d1?auto=format&fit=crop&w=1506&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" className="splash-image" alt="Shelf of records"></img>
-            </div>
         </div>
     );
 }
