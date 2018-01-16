23ab11c9a60d4eeedb85008b1ca8dfd17861b0b2
Jesse Shaw <jdshaw1987@gmail.com>
2018-01-16 12:11:30 -0500

    Updating Redux Form and Auth for signup and login

--- src/auth/register/signup.js
+++ src/auth/register/signup.js
@@ -3,27 +3,37 @@ import {Field, reduxForm, focus} from 'redux-form';
 import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
 import Input from '../input'
 
-export default function Signup() {
-    return (
-        <section className="signup-form-section">
-        <h2>Sign Up</h2>
-        <div className="signup-form">
-            <form>
-                <label htmlFor="fname">First Name</label>
-                <Field component={Input} id="fname" name="firstname" placeholder="Your name.." />
-                <label htmlFor="lname">Last Name</label>
-                <Field component={Input} id="lname" name="lastname" placeholder="Your last name.." />
-                <label htmlFor="fname">Username</label>
-                <Field component={Input} id="fname" name="firstname" placeholder="Your username.." />
-                <label htmlFor="lname">Email</label>
-                <Field component={Input} id="lname" name="lastname" placeholder="Your email.." />
-                <label htmlFor="fname">Password</label>
-                <Field component={Input} id="fname" name="firstname" placeholder="Your password.." />
-                <label htmlFor="lname">Confirm Password</label>
-                <Field component={Input} id="lname" name="lastname" placeholder="Your password again.." />
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-        </section>
-    );
+export class Signup extends React.Component {
+    onSubmit(values) {
+        console.log(values);
+    }
+    render() {
+        return (
+            <section className="signup-form-section">
+            <h2>Sign Up</h2>
+            <div className="signup-form">
+                <form>
+                    <label htmlFor="fname">First Name</label>
+                    <Field component={Input} id="fname" name="firstname" placeholder="Your name.." />
+                    <label htmlFor="lname">Last Name</label>
+                    <Field component={Input} id="lname" name="lastname" placeholder="Your last name.." />
+                    <label htmlFor="fname">Username</label>
+                    <Field component={Input} id="fname" name="firstname" placeholder="Your username.." />
+                    <label htmlFor="lname">Email</label>
+                    <Field component={Input} id="lname" name="lastname" placeholder="Your email.." />
+                    <label htmlFor="fname">Password</label>
+                    <Field component={Input} id="fname" name="firstname" placeholder="Your password.." />
+                    <label htmlFor="lname">Confirm Password</label>
+                    <Field component={Input} id="lname" name="lastname" placeholder="Your password again.." />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+            </section>
+        );
+    }
+
 }
+
+export default reduxForm({
+    form: 'signup'
+})(Signup);
