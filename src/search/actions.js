96dd7f276cfe8c43280d359add06aaf2097daeb4
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 11:34:23 -0400

    Updated Search settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -27,11 +27,19 @@ function _search(name) {
     }
     let artist = name;
     let searchTerm = artist.split(' ').join('+');
-    if (searchTerm.search("$") !== -1) {
-        console.log("Need to replace $")
-    }
     return fetch(`
-        ${itunesUrl}${searchTerm}&country=no`
+        ${itunesUrl}${searchTerm}&country=no`, {
+            method: "GET", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, same-origin, *omit
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "client" // no-referrer, *client
+        }
     ).then(function (response){
         return response.json();
     })
