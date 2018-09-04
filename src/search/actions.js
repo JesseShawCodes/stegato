dc101f9b8154744b72785827d2ef37a47b00b82e
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 11:35:51 -0400

    Updated Search settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -30,8 +30,6 @@ function _search(name) {
     return fetch(`
         ${itunesUrl}${searchTerm}&country=no`, {
             method: "GET", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, cors, *same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             credentials: "same-origin", // include, same-origin, *omit
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
