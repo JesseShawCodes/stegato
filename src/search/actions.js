79a68aa27a67ce60bcdae5bd8636a0b47b670c8e
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 12:36:20 -0400

    Updated Search Functionality

--- src/search/actions.js
+++ src/search/actions.js
@@ -27,7 +27,18 @@ function _search(name) {
     }
     let artist = name;
     let searchTerm = artist.split(' ').join('+');
-    return fetch(`https://itunes.apple.com/search?term=${searchTerm}&entity=album`)
+    return fetch(`https://itunes.apple.com/search?term=${searchTerm}&entity=album`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "omit", // include, same-origin, *omit
+        headers: {
+            "Content-Type": "application/javascript; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "client" // no-referrer, *client
+    })
     .then(function(res) {
         if (!res.ok) {
             return Promise.reject(res.statusText);
