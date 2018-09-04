1d724960a1079110ff962f08fbf91158e6ad4dc3
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 10:29:33 -0400

    Updated FETCH settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -38,8 +38,7 @@ function _search(name) {
                 // "Content-Type": "application/x-www-form-urlencoded",
             },
             redirect: "follow", // manual, *follow, error
-            referrer: "client", // no-referrer, *client
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
+            referrer: "client" // no-referrer, *client
         }
     ).then(function (response){
         return response.json();
