5c38d02d3ed121f3345018f3201a805a92f45f00
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 13:28:49 -0400

    Updated FETCH Settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -27,17 +27,8 @@ function _search(name) {
     }
     let artist = name;
     let searchTerm = artist.split(' ').join('+');
-    return fetch(`https://itunes.apple.com/search?term=${searchTerm}&entity=album&country=no`, {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "omit", // include, same-origin, *omit
-        headers: {
-            "Content-Type": "application/javascript; charset=utf-8",
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "client" // no-referrer, *client
+    return fetch(`http://itunes.apple.com/search?term=${searchTerm}&entity=album&country=no`, {
+        method: "GET" // *GET, POST, PUT, DELETE, etc.
     })
     .then(function(res) {
         if (!res.ok) {
