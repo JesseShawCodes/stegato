6c86731e9bc6683bcbf65f12788164b356d823fd
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 11:06:47 -0400

    Updated FETCH settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -28,7 +28,7 @@ function _search(name) {
     let artist = name;
     let searchTerm = artist.split(' ').join('+');
     return fetch(`
-        ${itunesUrl}${searchTerm}`, {
+        ${itunesUrl}${searchTerm}&country=no`, {
             method: "GET", // *GET, POST, PUT, DELETE, etc.
             mode: "no-cors", // no-cors, cors, *same-origin
             cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
