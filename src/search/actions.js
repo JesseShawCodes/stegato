d57a595e4b73bff9783cdb40b79519fba8575494
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 10:47:25 -0400

    Updated FETCH settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -30,7 +30,7 @@ function _search(name) {
     return fetch(`
         ${itunesUrl}${searchTerm}`, {
             method: "GET", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, cors, *same-origin
+            mode: "no-cors", // no-cors, cors, *same-origin
             cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
             credentials: "same-origin", // include, same-origin, *omit
             headers: {
