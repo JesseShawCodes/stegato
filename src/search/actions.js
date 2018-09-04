e350e26f5c18a377a9e95188852327fccf4fd103
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 12:50:48 -0400

    Updated Search Settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -29,7 +29,7 @@ function _search(name) {
     let searchTerm = artist.split(' ').join('+');
     return fetch(`https://itunes.apple.com/search?term=${searchTerm}&entity=album&country=no`, {
         method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "no-cors", // no-cors, cors, *same-origin
+        mode: "same-origin", // no-cors, cors, *same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: "omit", // include, same-origin, *omit
         headers: {
