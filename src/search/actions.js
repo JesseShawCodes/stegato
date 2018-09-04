93405866cd739bc9093d5af4bb4cf42bfff22f0e
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 10:00:09 -0400

    Updated Mode in initial fetch

--- src/search/actions.js
+++ src/search/actions.js
@@ -29,7 +29,7 @@ function _search(name) {
     let searchTerm = artist.split(' ').join('+');
     return fetch(`
         ${itunesUrl}${searchTerm}`, {
-            mode: "no-cors", // no-cors, cors, *same-origin
+            mode: "*same-origin", // no-cors, cors, *same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             credentials: "same-origin", // include, same-origin, *omit
             headers: {
