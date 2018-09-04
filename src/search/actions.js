56c4b4a4803a85d26ceac2f30abef985fecf7301
Jesse Shaw <jdshaw1987@gmail.com>
2018-09-04 09:39:21 -0400

    Updated Action Settings

--- src/search/actions.js
+++ src/search/actions.js
@@ -27,7 +27,21 @@ function _search(name) {
     }
     let artist = name;
     let searchTerm = artist.split(' ').join('+');
-    return fetch(`${itunesUrl}${searchTerm}`).then(function (response){
+    return fetch(`
+        ${itunesUrl}${searchTerm}`, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, same-origin, *omit
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        }
+    ).then(function (response){
         return response.json();
     })
     .then(function (json){
